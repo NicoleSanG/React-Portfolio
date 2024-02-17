3a30183015f347234533912bd63f399ae92beee1
NicoleSG <nicolesantamariagranizo@gmail.com>
2024-02-17 21:56:02 +0000

    added style to nav bar and media queries

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,39 @@
 import './NavbarStyles.css';
 
-import React from 'react';
-import {Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
-  return (
-    <div className='header'>
-      <Link to="/">
-        <h1>NS</h1>
-      </Link>
-      <ul className='nav-menu'>
-        <li>
-            <Link to="/">Home</Link>
-        </li>
-        <li>
-            <Link to="/about">About</Link>
-        </li>
-        <li>
-            <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-            <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </div>
-  );
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    return (
+        <div className='header'>
+            <Link to="/">
+                <h1>NS</h1>
+            </Link>
+            <ul className={click ?'nav-menu active' : 'nav-menu'}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            <div className='hamburger' onClick= {handleClick}>
+                {click ? (<FaTimes />) :
+                (<FaBars/>)}
+            </div>
+        </div>
+    );
 };
 
 export default Navbar
